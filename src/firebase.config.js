323c40d00526c567a5cd72b5c4e8e23587ddfa98
abcdevelop <bruno.panaget@yahoo.fr>
2018-03-15 11:33:03 +0100

    improvements

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,12 +1,7 @@
 import Firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyCn3PR-rlqb9SVeuFoaVus6zD2htSh9uGM",
-  authDomain: "infos-tools.firebaseapp.com",
-  databaseURL: "https://infos-tools.firebaseio.com",
-  projectId: "infos-tools",
-  storageBucket: "infos-tools.appspot.com",
-  messagingSenderId: "98555307856"
+
 };
 const firebaseApp = Firebase.initializeApp(config);
 const db = firebaseApp.database()
